022865a317116131f641e32c915d2a50f574badd
Eli Hermann <Eli_Hermann@hotmail.com>
2021-10-14 23:46:19 -0600

    working on windows and has parallelization

--- sortutils.cpp
+++ sortutils.cpp
@@ -16,7 +16,7 @@ void organPipeStdArray(SourceArray& data){
     }
 }
 
-void timeRawArray(SourceArray sa)
+void timeRawArraySeq(SourceArray sa)
 {
     std::chrono::duration<double> totalTime = std::chrono::steady_clock::duration::zero();
     for(int i=0; i < HOW_MANY_TIMES; i++)
@@ -24,7 +24,22 @@ void timeRawArray(SourceArray sa)
         int raw[250000];
         initializeRawArrayFromStdArray(sa, raw);
         auto start = std::chrono::steady_clock::now(); // start timer
-        std::sort(std::execution::seq ,raw, raw + 250000);
+        std::sort(std::execution::seq,raw, raw + 250000);
+        auto end = std::chrono::steady_clock::now(); // end timer
+        totalTime += end - start;
+    }
+    std::cout << "Sort took: " << totalTime.count() << " seconds" << std::endl;
+}
+
+void timeRawArrayPar(SourceArray sa)
+{
+    std::chrono::duration<double> totalTime = std::chrono::steady_clock::duration::zero();
+    for (int i = 0; i < HOW_MANY_TIMES; i++)
+    {
+        int raw[250000];
+        initializeRawArrayFromStdArray(sa, raw);
+        auto start = std::chrono::steady_clock::now(); // start timer
+        std::sort(std::execution::par, raw, raw + 250000);
         auto end = std::chrono::steady_clock::now(); // end timer
         totalTime += end - start;
     }
@@ -32,14 +47,22 @@ void timeRawArray(SourceArray sa)
 }
 
 void evaluateRawArray(const SourceArray& random, const SourceArray& sorted, const SourceArray& reversed, const SourceArray& organPipe, const SourceArray& rotated){
-    timeRawArray(random);
-    timeRawArray(sorted);
-    timeRawArray(reversed);		
-    timeRawArray(organPipe);
-    timeRawArray(rotated);
+    std::cout << "Sequential" << std::endl;
+    timeRawArraySeq(random);
+    timeRawArraySeq(sorted);
+    timeRawArraySeq(reversed);
+    timeRawArraySeq(organPipe);
+    timeRawArraySeq(rotated);
+
+    std::cout << "Parallel" << std::endl;
+    timeRawArrayPar(random);
+    timeRawArrayPar(sorted);
+    timeRawArrayPar(reversed);
+    timeRawArrayPar(organPipe);
+    timeRawArrayPar(rotated);
 }
 
-void timeStdArray(SourceArray source){
+void timeStdArraySeq(SourceArray source){
 
     std::chrono::duration<double> totalTime = std::chrono::steady_clock::duration::zero();
     for(int i=0; i < HOW_MANY_TIMES; i++)
@@ -47,7 +70,22 @@ void timeStdArray(SourceArray source){
         std::array<int, 250000> sa;
         std::copy(source.begin(), source.end(), sa.begin());
         auto start = std::chrono::steady_clock::now(); // start timer
-        std::sort(sa.begin(), sa.end());
+        std::sort(std::execution::seq, sa.begin(), sa.end());
+        auto end = std::chrono::steady_clock::now(); // end timer
+        totalTime += end - start;
+    }
+    std::cout << "Sort took: " << totalTime.count() << " seconds" << std::endl;
+}
+
+void timeStdArrayPar(SourceArray source) {
+
+    std::chrono::duration<double> totalTime = std::chrono::steady_clock::duration::zero();
+    for (int i = 0; i < HOW_MANY_TIMES; i++)
+    {
+        std::array<int, 250000> sa;
+        std::copy(source.begin(), source.end(), sa.begin());
+        auto start = std::chrono::steady_clock::now(); // start timer
+        std::sort(std::execution::par, sa.begin(), sa.end());
         auto end = std::chrono::steady_clock::now(); // end timer
         totalTime += end - start;
     }
@@ -55,14 +93,22 @@ void timeStdArray(SourceArray source){
 }
 
 void evaluateStdArray(const SourceArray& random, const SourceArray& sorted, const SourceArray& reversed, const SourceArray& organPipe, const SourceArray& rotated){
-    timeStdArray(random);
-    timeStdArray(sorted);
-    timeStdArray(reversed);		
-    timeStdArray(organPipe);
-    timeStdArray(rotated);
+    std::cout << "Sequential" << std::endl;
+    timeStdArraySeq(random);
+    timeStdArraySeq(sorted);
+    timeStdArraySeq(reversed);		
+    timeStdArraySeq(organPipe);
+    timeStdArraySeq(rotated);
+
+    std::cout << "Parallel" << std::endl;
+    timeStdArrayPar(random);
+    timeStdArrayPar(sorted);
+    timeStdArrayPar(reversed);
+    timeStdArrayPar(organPipe);
+    timeStdArrayPar(rotated);
 }
 
-void timeStdVector(SourceArray source)
+void timeStdVectorSeq(SourceArray source)
 {
     std::chrono::duration<double> totalTime = std::chrono::steady_clock::duration::zero();
     for(int i=0; i < HOW_MANY_TIMES; i++)
@@ -70,7 +116,22 @@ void timeStdVector(SourceArray source)
         std::vector<int> vector;
         std::copy(source.begin(), source.end(), std::back_inserter(vector));
         auto start = std::chrono::steady_clock::now(); // start timer
-        std::sort(vector.begin(), vector.end());
+        std::sort(std::execution::seq, vector.begin(), vector.end());
+        auto end = std::chrono::steady_clock::now(); // end timer
+        totalTime += end - start;
+    }
+    std::cout << "Sort took: " << totalTime.count() << " seconds" << std::endl;
+}
+
+void timeStdVectorPar(SourceArray source)
+{
+    std::chrono::duration<double> totalTime = std::chrono::steady_clock::duration::zero();
+    for (int i = 0; i < HOW_MANY_TIMES; i++)
+    {
+        std::vector<int> vector;
+        std::copy(source.begin(), source.end(), std::back_inserter(vector));
+        auto start = std::chrono::steady_clock::now(); // start timer
+        std::sort(std::execution::par, vector.begin(), vector.end());
         auto end = std::chrono::steady_clock::now(); // end timer
         totalTime += end - start;
     }
@@ -78,9 +139,17 @@ void timeStdVector(SourceArray source)
 }
 
 void evaluateStdVector(const SourceArray& random, const SourceArray& sorted, const SourceArray& reversed, const SourceArray& organPipe, const SourceArray& rotated){
-    timeStdVector(random);
-    timeStdVector(sorted);
-    timeStdVector(reversed);		
-    timeStdVector(organPipe);
-    timeStdVector(rotated);
+    std::cout << "Sequential" << std::endl;
+    timeStdVectorSeq(random);
+    timeStdVectorSeq(sorted);
+    timeStdVectorSeq(reversed);		
+    timeStdVectorSeq(organPipe);
+    timeStdVectorSeq(rotated);
+
+    std::cout << "Parallel" << std::endl;
+    timeStdVectorPar(random);
+    timeStdVectorPar(sorted);
+    timeStdVectorPar(reversed);
+    timeStdVectorPar(organPipe);
+    timeStdVectorPar(rotated);
 }
